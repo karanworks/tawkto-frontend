f90a3c4084562f1ea771344d8ed841d0e83f74dc
Zahid Raza <intimidatorthe@gmail.com>
2024-11-06 14:53:17 +0530

    column removed

--- src/pages/Settings/General/index.js
+++ src/pages/Settings/General/index.js
@@ -44,163 +44,7 @@ const General = () => {
         <Container fluid>
           <BreadCrumb title="Settings" pageTitle="General" />
 
-          {/* <div className="position-relative mx-n4 mt-n4"> */}
-          {/* <div className="profile-wid-bg profile-setting-img">
-              
-              <div className="overlay-content">
-                <div className="text-end p-3">
-                  <div className="p-0 ms-auto rounded-circle profile-photo-edit">
-                    <Input
-                      id="profile-foreground-img-file-input"
-                      type="file"
-                      className="profile-foreground-img-file-input"
-                    />
-                    <Label
-                      htmlFor="profile-foreground-img-file-input"
-                      className="profile-photo-edit btn btn-light"
-                    >
-                      <i className="ri-image-edit-line align-bottom me-1"></i>{" "}
-                      Change Cover
-                    </Label>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-          {/* </div> */}
           <Row>
-            <Col xxl={3}>
-              {/* <Card className="mt-n5">
-                <CardBody className="p-4">
-                  <div className="text-center">
-                    <div className="profile-user position-relative d-inline-block mx-auto  mb-4">
-                     
-                      <div className="avatar-xs p-0 rounded-circle profile-photo-edit">
-                        <Input
-                          id="profile-img-file-input"
-                          type="file"
-                          className="profile-img-file-input"
-                        />
-                        <Label
-                          htmlFor="profile-img-file-input"
-                          className="profile-photo-edit avatar-xs"
-                        >
-                          <span className="avatar-title rounded-circle bg-light text-body">
-                            <i className="ri-camera-fill"></i>
-                          </span>
-                        </Label>
-                      </div>
-                    </div>
-                    <h5 className="fs-16 mb-1">Anna Adame</h5>
-                    <p className="text-muted mb-0">Lead Designer / Developer</p>
-                  </div>
-                </CardBody>
-              </Card> */}
-
-              <Card>
-                <CardBody>
-                  <div className="d-flex align-items-center mb-5">
-                    <div className="flex-grow-1">
-                      <h5 className="card-title mb-0">Complete Your Profile</h5>
-                    </div>
-                    <div className="flex-shrink-0">
-                      <Link
-                        to="#"
-                        className="badge bg-light text-secondary fs-12"
-                      >
-                        <i className="ri-edit-box-line align-bottom me-1"></i>{" "}
-                        Edit
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="progress animated-progress custom-progress progress-label">
-                    <div
-                      className="progress-bar bg-primary"
-                      role="progressbar"
-                      style={{ width: "30%" }}
-                      aria-valuenow="30"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    >
-                      <div className="label">30%</div>
-                    </div>
-                  </div>
-                </CardBody>
-              </Card>
-              {/* <Card>
-                <CardBody>
-                  <div className="d-flex align-items-center mb-4">
-                    <div className="flex-grow-1">
-                      <h5 className="card-title mb-0">Portfolio</h5>
-                    </div>
-                    <div className="flex-shrink-0">
-                      <Link
-                        to="#"
-                        className="badge bg-light text-secondary fs-12"
-                      >
-                        <i className="ri-add-fill align-bottom me-1"></i> Add
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="mb-3 d-flex">
-                    <div className="avatar-xs d-block flex-shrink-0 me-3">
-                      <span className="avatar-title rounded-circle fs-16 bg-body text-body">
-                        <i className="ri-github-fill"></i>
-                      </span>
-                    </div>
-                    <Input
-                      type="email"
-                      className="form-control"
-                      id="gitUsername"
-                      placeholder="Username"
-                      defaultValue="@daveadame"
-                    />
-                  </div>
-                  <div className="mb-3 d-flex">
-                    <div className="avatar-xs d-block flex-shrink-0 me-3">
-                      <span className="avatar-title rounded-circle fs-16 bg-primary-subtle text-primary">
-                        <i className="ri-global-fill"></i>
-                      </span>
-                    </div>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      id="websiteInput"
-                      placeholder="www.example.com"
-                      defaultValue="www.velzon.com"
-                    />
-                  </div>
-                  <div className="mb-3 d-flex">
-                    <div className="avatar-xs d-block flex-shrink-0 me-3">
-                      <span className="avatar-title rounded-circle fs-16 bg-success-subtle text-success">
-                        <i className="ri-dribbble-fill"></i>
-                      </span>
-                    </div>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      id="dribbleName"
-                      placeholder="Username"
-                      defaultValue="@dave_adame"
-                    />
-                  </div>
-                  <div className="d-flex">
-                    <div className="avatar-xs d-block flex-shrink-0 me-3">
-                      <span className="avatar-title rounded-circle fs-16 bg-danger-subtle text-danger">
-                        <i className="ri-pinterest-fill"></i>
-                      </span>
-                    </div>
-                    <Input
-                      type="text"
-                      className="form-control"
-                      id="pinterestName"
-                      placeholder="Username"
-                      defaultValue="Advance Dave"
-                    />
-                  </div>
-                </CardBody>
-              </Card> */}
-            </Col>
-
             <Col xxl={9}>
               <Card className="mt-xxl-n5">
                 <CardHeader>
@@ -219,45 +63,6 @@ const General = () => {
                         Personal Details
                       </NavLink>
                     </NavItem>
-                    {/* <NavItem>
-                      <NavLink
-                        to="#"
-                        className={classnames({ active: activeTab === "2" })}
-                        onClick={() => {
-                          tabChange("2");
-                        }}
-                        type="button"
-                      >
-                        <i className="far fa-user"></i>
-                        Change Password
-                      </NavLink>
-                    </NavItem> */}
-                    {/* <NavItem>
-                      <NavLink
-                        to="#"
-                        className={classnames({ active: activeTab === "3" })}
-                        onClick={() => {
-                          tabChange("3");
-                        }}
-                        type="button"
-                      >
-                        <i className="far fa-envelope"></i>
-                        Experience
-                      </NavLink>
-                    </NavItem> */}
-                    {/* <NavItem>
-                      <NavLink
-                        to="#"
-                        className={classnames({ active: activeTab === "4" })}
-                        onClick={() => {
-                          tabChange("4");
-                        }}
-                        type="button"
-                      >
-                        <i className="far fa-envelope"></i>
-                        Privacy Policy
-                      </NavLink>
-                    </NavItem> */}
                   </Nav>
                 </CardHeader>
                 <CardBody className="p-4">
@@ -329,57 +134,7 @@ const General = () => {
                               />
                             </div>
                           </Col>
-                          {/* <Col lg={12}>
-                            <div className="mb-3">
-                              <Label
-                                htmlFor="JoiningdatInput"
-                                className="form-label"
-                              >
-                                Joining Date
-                              </Label>
-                              <Flatpickr
-                                className="form-control"
-                                options={{
-                                  dateFormat: "d M, Y",
-                                }}
-                              />
-                            </div>
-                          </Col> */}
-                          {/* <Col lg={12}>
-                            <div className="mb-3">
-                              <Label
-                                htmlFor="skillsInput"
-                                className="form-label"
-                              >
-                                Skills
-                              </Label>
-                              <select className="form-select mb-3">
-                                <option>Select your Skill </option>
-                                <option value="Choices1">CSS</option>
-                                <option value="Choices2">HTML</option>
-                                <option value="Choices3">PYTHON</option>
-                                <option value="Choices4">JAVA</option>
-                                <option value="Choices5">ASP.NET</option>
-                              </select>
-                            </div>
-                          </Col> */}
-                          {/* <Col lg={6}>
-                            <div className="mb-3">
-                              <Label
-                                htmlFor="designationInput"
-                                className="form-label"
-                              >
-                                Designation
-                              </Label>
-                              <Input
-                                type="text"
-                                className="form-control"
-                                id="designationInput"
-                                placeholder="Designation"
-                                defaultValue="Lead Designer / Developer"
-                              />
-                            </div>
-                          </Col> */}
+
                           <Col lg={6}>
                             <div className="mb-3">
                               <Label
@@ -443,23 +198,7 @@ const General = () => {
                               />
                             </div>
                           </Col>
-                          {/* <Col lg={12}>
-                            <div className="mb-3 pb-2">
-                              <Label
-                                htmlFor="exampleFormControlTextarea"
-                                className="form-label"
-                              >
-                                Description
-                              </Label>
-                              <textarea
-                                className="form-control"
-                                id="exampleFormControlTextarea"
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                                rows="3"
-                              />
-                            </div>
-                          </Col> */}
+
                           <Col lg={12}>
                             <div className="hstack gap-2 justify-content-end">
                               <button type="button" className="btn btn-primary">
